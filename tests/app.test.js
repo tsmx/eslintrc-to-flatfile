119973cf235d48302bb50716a789e4117c7a28ab
tsmx <dev@tsmx.net>
2024-08-18 21:32:39 +0200

    Typo fixed

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,7 @@
 const addOne = require('../app');
 const { testEquality } = require('./test-utils');
 
-describe('seslintrc-to-flatfile test suite', () => {
+describe('eslintrc-to-flatfile test suite', () => {
 
     it('tests addOne function', () => {
         testEquality(addOne(4), 5);
